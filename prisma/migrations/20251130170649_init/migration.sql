34395e6fa6a48f4b85aff13a856b19e7564f8fbe
sstrodee <realmayzex@gmail.com>
2025-11-30 22:22:18 +0500

    feat:

--- prisma/migrations/20251130170649_init/migration.sql
+++ prisma/migrations/20251130170649_init/migration.sql
@@ -1,14 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Todo";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "categories" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
